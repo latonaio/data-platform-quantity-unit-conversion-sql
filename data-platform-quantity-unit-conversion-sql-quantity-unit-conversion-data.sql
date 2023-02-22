bb093da64450f39410d4f639282e3eb24286f267
MiyuYokoyama0109 <miyu.y@latona.io>
2023-02-22 13:16:53 +0900

    release

--- data-platform-quantity-unit-conversion-sql-quantity-unit-conversion-data.sql
+++ data-platform-quantity-unit-conversion-sql-quantity-unit-conversion-data.sql
@@ -1,9 +1,13 @@
-CREATE TABLE `data_platform_quantity_conversion_quantity_unit_conversion_data`
+CREATE TABLE `data_platform_quantity_unit_conversion_quantity_unit_conversion_data`
 (
     `QuantityUnitFrom`                     varchar(3) NOT NULL,  
     `QuantityUnitTo`                       varchar(3) NOT NULL,  
     `ConversionCoefficient`                float(24) NOT NULL,  
 
-    PRIMARY KEY (`QuantityUnitFrom`, `QuantityUnitTo`, `ConversionCoefficient`)
+    PRIMARY KEY (`QuantityUnitFrom`, `QuantityUnitTo`),
+
+    CONSTRAINT `DataPlatformQuantityUnitConversionQuantityUnitConversionDataQuantityUnitFrom_fk` FOREIGN KEY (`QuantityUnitFrom`) REFERENCES `data_platform_quantity_unit_quantity_unit_data` (`QuantityUnit`),
+    CONSTRAINT `DataPlatformQuantityUnitConversionQuantityUnitConversionDataQuantityUnitTo_fk` FOREIGN KEY (`QuantityUnitTo`) REFERENCES `data_platform_quantity_unit_quantity_unit_data` (`QuantityUnit`)
+
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
